807b0ddf30baf6fd9df70992d21dbd4bdde2f456
Daniel Colangelo <Danalfons@yahoo.com>
2017-02-15 07:19:08 -0500

    more testing
    more testing with swift 3

---
+++ swiftBasics.playground/Pages/types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+let country = "United States of America"
+
+let state = "Florida"
+
+let city = "St Pete Beach"
+
+// string concatination
+
+let address = country + state + city
+
+// string interpolation
+
+let interpolatedAddress = "\(country) \(state) \(city)"
+
+//exanple of interpolation
+
+let name = "Danny"
+let greeting = "Hi there, \(name)."
+let finalGreeting = "\(greeting) How are you?"
+
+// Int and numbers
+
